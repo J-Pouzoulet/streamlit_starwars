131ad7535606d0e118e76e011220008ba55eb399
jerome pouzoulet <jerome.pouzoulet@gmail.com>
2023-01-06 09:53:50 +0100

    json fixed

--- app/app.py
+++ app/app.py
@@ -3,19 +3,10 @@ import pandas as pd
 import numpy as np
 import requests as rq
 import json
-from PIL import Image
-
-#image = Image.open('home/jerome/code/J-Pouzoulet/IA3/my_first_streamlit/data/wallpaper.jpg')
-#image = Image('../data/wallpaper.jpg')
-#st.image(image)
 
 st.image('../data/wallpaper.jpg')
 
 st.title(f'Hello *Geeks!!!* :sunglasses:')
-
-#input = ('r2')
-
-#req = rq.request(f'https://swapi.dev/api/people/?search={''}')
 
 col1, col2 = st.columns(2)
 
@@ -30,8 +21,6 @@ with col2:
     text_input = st.text_input(
         f"Search about StarWars {option} here 👇",''
     )
-
-st.write(option == '', text_input == '')
 
 if option == '' or text_input == '':
     pass
@@ -74,51 +63,3 @@ else:
             for item in res['results'][dict_title[title]].keys():
                 obj = res['results'][dict_title[title]][item]
                 st.write(f'{item} : {obj}')
-
-
-#else:       
-    #res = json.loads(rq.get(f'https://swapi.dev/api/{option}/?search={text_input}').text) 
-    #st.write(res)
-
-#st.markdown(f"# Akinator : la clé : {st.secrets.api_key}")
-
-#st.image("https://fr.akinator.com/bundles/elokencesite/images/akitudes_670x1096/defi.png", width=200)
-
-
-#st.markdown("## Description")
- 
-#st.markdown('Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sunt quod porro nulla omnis nam non earum debitis consectetur ut recusandae, sed quaerat qui iste? Delectus non minima minus dolorum.')
-
-#st.markdown('<a src="https://docs.streamlit.io/library/api-reference">Doc</a>', unsafe_allow_html = True)
-
-#st.latex(r'''
-#    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-#    \sum_{k=0}^{n-1} ar^k =
-#    a \left(\frac{1-r^{n}}{1-r}\right)
-#    ''')
-
-
-#df = pd.DataFrame({
-#    'first column': list(range(1, 11)),
-#    'second column': np.arange(10, 101, 10)
-#})
-
-#df['thirth column'] = df['first column'] * 2
-
-
-#x1 = st.slider('x?', 1, 11, 3)
-#y1 = st.slider('y?', 1, 11, 3)
-
-#def sum(x, y):
-#    return x + y
-    
-#st.write(f"La somme de {x1} et de {y1} est {sum(x1, y1)}")
-
-
-#lines_number = st.slider('Lines number?', 1, 11, 3)
-
-#head_df = df.head(lines_number)
-
-#head_df
-
-#st.line_chart(df)
